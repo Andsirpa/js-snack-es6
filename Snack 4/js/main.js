f955c46fcf614bbada7e3d601bb065fc6053601b
Andsirpa <andsirpa94@gmail.com>
2024-01-25 15:19:40 +0100

    feat: Snack 4

--- Snack 4/js/main.js
+++ Snack 4/js/main.js
@@ -36,3 +36,30 @@ const teams = [
     },
 ]
 
+// genero i valori randomico da 1 a 10 per ogni team
+
+for (const team of teams) {
+    // valore randomico per ogni score
+    team.score = Math.floor(Math.random() * 11)
+    // valore randomico per ogni foul
+    team.foul = Math.floor(Math.random() * 11)
+
+}
+
+
+const container = document.getElementById('info-container')
+
+// creo un 'div' per raccogliere le info in un 'p'
+const output = document.createElement('div')
+
+// uso il forEach e il destructuring per recuperare i valori dall'oggetto
+teams.forEach(({ name, score, foul }) => output.innerHTML += `<p>${name} : Score = ${score}, Foul = ${foul}</p> <br>`)
+// stampo l'output in console
+console.log(output);
+
+
+// aggiungo l'output al container per stampare in pagina
+container.appendChild(output)
+
+
+
