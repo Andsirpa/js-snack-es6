b57749b3052eb06f07f0b742e944fa60b92f1dc6
Andsirpa <andsirpa94@gmail.com>
2024-01-25 08:59:11 +0100

    feat: Snack 1

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -1,1 +1,23 @@
-console.log('JS OK');
+// array di ospiti
+const vips = [
+    'Dwayne Johnson',
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin',
+];
+
+// dichiaro la costante table ( uguale per tutti gli ospiti )
+const tableName = "Tavolo Vip"
+
+// return implicito,                                       restituisco implicitamente il valore 
+const segnaPosto = vips.map((guest, index) => ({ tableName, guestName: guest, seatNumber: index + 1 }))
+
+// stampo 
+console.log(segnaPosto)
